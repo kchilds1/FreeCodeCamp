420ee7bd8202d5f8f340a612b1c47f3ed4d911f9
Kenneth Childs Jr <103333336+kchilds1@users.noreply.github.com>
2023-05-21 14:03:08 -0500

    Update index.js

--- JavaScript Algorithms and Data Structures Certification/functional Programming/Remove Elements from an Array Using slice Instead of splice/index.js
+++ JavaScript Algorithms and Data Structures Certification/functional Programming/Remove Elements from an Array Using slice Instead of splice/index.js
@@ -1,1 +1,11 @@
-
+function nonMutatingSplice(cities) {
+    // Only change code below this line
+  const locations = cities.slice(0, 3);
+    return locations;
+  
+    // Only change code above this line
+  }
+  
+  const inputCities = ["Chicago", "Delhi", "Islamabad", "London", "Berlin"];
+  nonMutatingSplice(inputCities);
+  console.log(nonMutatingSplice(inputCities));
