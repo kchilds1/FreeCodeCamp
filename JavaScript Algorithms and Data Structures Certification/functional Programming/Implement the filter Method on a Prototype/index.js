5aef7d1beb550d0efeb42a1f3b388cd4ddae0fca
Kenneth Childs Jr <103333336+kchilds1@users.noreply.github.com>
2023-05-21 13:09:37 -0500

    Update index.js

--- JavaScript Algorithms and Data Structures Certification/functional Programming/Implement the filter Method on a Prototype/index.js
+++ JavaScript Algorithms and Data Structures Certification/functional Programming/Implement the filter Method on a Prototype/index.js
@@ -1,1 +1,16 @@
-
+Array.prototype.myFilter = function(callback) {
+    const newArray = [];
+    // Only change code below this line
+    //created a for loop to loop through all the elements
+  for(let i = 0; i < this.length; i++){
+    //create an if statement to callback the function being passed as an argument, access the elements at index i in the this array.
+    if(callback(this[i], i, this) == true){
+        //if if statement is true the element will be pushed to newArray. This process will keep going until we for loop through all elements in this array.
+    newArray.push(this[i]);
+    }
+  }
+    // Only change code above this line
+    return newArray;
+  };
+  
+  console.log([23, 65, 98, 5, 13].myFilter(item => item % 2))
