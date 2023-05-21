d6288d7c085eed28caf2dbc6ed4ac9ed4fd5fd14
Kenneth Childs Jr <103333336+kchilds1@users.noreply.github.com>
2023-05-21 13:42:43 -0500

    Update index.js

--- JavaScript Algorithms and Data Structures Certification/functional Programming/Return Part of an Array Using the slice Method/index.js
+++ JavaScript Algorithms and Data Structures Certification/functional Programming/Return Part of an Array Using the slice Method/index.js
@@ -1,1 +1,12 @@
+function sliceArray(anim, beginSlice, endSlice) {
+    // Only change code below this line
+  //slice method used to element with index 1 to element with index 3.  In this case it would be Dog and Tiger going into new Array
+    const newArray = anim.slice(beginSlice, endSlice);
 
+    return newArray;
+    // Only change code above this line
+  }
+  
+  const inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
+  sliceArray(inputAnim, 1, 3);
+  console.log(sliceArray(inputAnim, 1, 3));
