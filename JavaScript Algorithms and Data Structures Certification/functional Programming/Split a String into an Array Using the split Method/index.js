8fff05a880876f7bd44dcc2d92d7f91dd4d91b34
Kenneth Childs Jr <103333336+kchilds1@users.noreply.github.com>
2023-05-22 11:07:21 -0500

    Update index.js

--- JavaScript Algorithms and Data Structures Certification/functional Programming/Split a String into an Array Using the split Method/index.js
+++ JavaScript Algorithms and Data Structures Certification/functional Programming/Split a String into an Array Using the split Method/index.js
@@ -1,10 +1,11 @@
 function splitify(str) {
-    // Only change code below this line
-    //the split is splitting the str parameter by using regular expressions for comma, dash, and space as the delimiters
-  const splitValue = str.split(/,|-| /);
-  return splitValue;
-  
-    // Only change code above this line
-  }
-  
-  console.log(splitify("Hello World,I-am code"));
+  // Only change code below this line
+const splitValue = str.split(/\.|,|-| /);
+return splitValue;
+
+  // Only change code above this line
+}
+
+console.log(splitify("Hello World,I-am code"));
+console.log(splitify("This.is.a-sentence"));
+console.log(splitify("Earth-is-our home"));
