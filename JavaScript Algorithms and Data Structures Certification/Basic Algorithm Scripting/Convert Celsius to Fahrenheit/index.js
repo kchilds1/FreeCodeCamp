0d644e6f938d5de0d8dcb40c62ae43705f21257f
Kenneth Childs Jr <103333336+kchilds1@users.noreply.github.com>
2023-05-23 11:00:11 -0500

    Create index.js

---
+++ JavaScript Algorithms and Data Structures Certification/Basic Algorithm Scripting/Convert Celsius to Fahrenheit/index.js
@@ -0,0 +1,11 @@
+function convertCtoF(celsius) {
+    //formula converts celsius to fahrenheit
+    let fahrenheit = (celsius * 9/5) + 32;
+    //return the fahrenheit value assigned to variable fahrenheit
+    return fahrenheit;
+  }
+  
+  console.log(convertCtoF(0));
+  console.log(convertCtoF(-10));
+  console.log(convertCtoF(20));
+  console.log(convertCtoF(30));
